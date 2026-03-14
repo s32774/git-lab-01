34c7b44d1261e71bd96fd3e8a2a9b8ccf64210e3
Olena Chornobai <s32774@pjwstk.edu.pl>
2026-03-14 11:52:06 +0100

    Add CalculateAverage method

--- ConsoleApp1/InithialProject/StatisticsHelper.cs
+++ ConsoleApp1/InithialProject/StatisticsHelper.cs
@@ -4,4 +4,21 @@
     {
         return a + b;
     }
+
+    public static double CalculateAverage(int[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            return 0;
+        }
+
+        int sum = 0;
+
+        foreach (int value in values)
+        {
+            sum += value;
+        }
+
+        return (double)sum / values.Length;
+    }
 }

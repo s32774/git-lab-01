411472d260c6fca539ba92284665f106dd822f41
Olena Chornobai <s32774@pjwstk.edu.pl>
2026-03-14 12:02:54 +0100

    Calculate max method

--- ConsoleApp1/InithialProject/StatisticsHelper.cs
+++ ConsoleApp1/InithialProject/StatisticsHelper.cs
@@ -21,4 +21,23 @@
 
         return (double)sum / values.Length;
     }
+    public static int CalculateMax(int[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            return 0;
+        }
+
+        int max = values[0];
+
+        foreach (int value in values)
+        {
+            if (value > max)
+            {
+                max = value;
+            }
+        }
+
+        return max;
+    }
 }

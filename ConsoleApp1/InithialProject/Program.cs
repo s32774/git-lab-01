00bf03a1dce07138bf2baa5a388534de37fec9a5
Olena Chornobai <s32774@pjwstk.edu.pl>
2026-03-14 11:36:33 +0100

    Add input validation

--- ConsoleApp1/InithialProject/Program.cs
+++ ConsoleApp1/InithialProject/Program.cs
@@ -1,7 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Statistics App");
 
 Console.Write("Enter first number: ");
 string first = Console.ReadLine();
 
 Console.Write("Enter second number: ");
 string second = Console.ReadLine();
+
+if (int.TryParse(first, out int a) && int.TryParse(second, out int b))
+{
+    Console.WriteLine("Sum = " + StatisticsHelper.Sum(a, b));
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
